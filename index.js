2cb56696c0f44b18cd384021271b4d512f430ef2
decokche <rmudi24@gmail.com>
2025-09-15 01:42:26 +0530

    signup routes created successfully

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
+import fileUpload from "express-fileupload"
+import bodyParser from "body-parser"
 
 import { ConnectDB } from "./config/db.config.js"
 import userRoutes from "./routes/user.routes.js"
@@ -9,6 +11,14 @@ const PORT = process.env.PORT || 8080
 
 const app = express()
 ConnectDB()
+
+app.use(bodyParser.json())
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+)
 
 app.use("/api/v1/user", userRoutes)
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,44 @@
 import express from "express"
 
+import bcrypt from "bcrypt"
+import mongoose from "mongoose"
+import User from "../models/user.model.js"
+import cloudinary from "../config/cloudinary.js"
+
 const router = express.Router()
 
-router.post("/signup", (req, res) => {
-  res.send("Hello bhai kya haal chaal aap log ka")
+router.post("/signup", async (req, res) => {
+  try {
+    console.log("request comming")
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    console.log(hashedPassword)
+    const uploadImage = await cloudinary.uploader.upload(
+      req.files.logoUrl.tempFilePath
+    )
+    console.log("IMAGE", uploadImage)
+
+    const newUser = new User({
+      _id: new mongoose.Types.ObjectId(),
+      email: req.body.email,
+      password: hashedPassword,
+      channelName: req.body.channelName,
+      phone: req.body.phone,
+      logoUrl: uploadImage.secure_url,
+      logoId: uploadImage.public_id,
+    })
+
+    let user = await newUser.save()
+
+    res.status(201).json({
+      user,
+    })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({
+      error: "Something went wrong",
+      message: error.message,
+    })
+  }
 })
 
 export default router

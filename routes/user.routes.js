1cd41fde05b8842756ecd1cbc40507adb182b70c
decokche <rmudi24@gmail.com>
2025-09-15 03:59:29 +0530

    /login route created successfully

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from "express"
-
+import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
 import mongoose from "mongoose"
 import User from "../models/user.model.js"
@@ -31,6 +31,59 @@ router.post("/signup", async (req, res) => {
 
     res.status(201).json({
       user,
+    })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({
+      error: "Something went wrong",
+      message: error.message,
+    })
+  }
+})
+
+router.post("/login", async (req, res) => {
+  try {
+    const existingUser = await User.findOne({ email: req.body.email })
+
+    if (!existingUser) {
+      return res.status(404).json({
+        message: "User not found",
+      })
+    }
+
+    const isValid = await bcrypt.compare(
+      req.body.password,
+      existingUser.password
+    )
+
+    if (!isValid) {
+      return res.status(400).json({
+        message: "Invalid credentials",
+      })
+    }
+
+    const token = jwt.sign(
+      {
+        _id: existingUser._id,
+        channelName: existingUser.channelName,
+        email: existingUser.email,
+        phone: existingUser.phone,
+        logoId: existingUser.logoId,
+      },
+      process.env.JWT_TOKEN,
+      { expiresIn: "10d" }
+    )
+
+    res.status(200).json({
+      _id: existingUser._id,
+      channelName: existingUser.channelName,
+      email: existingUser.email,
+      phone: existingUser.phone,
+      logoId: existingUser.logoId,
+      logoUrl: existingUser.logoUrl,
+      token: token,
+      subscribers: existingUser.subscribers,
+      subscribedChannels: existingUser.subscribedChannels,
     })
   } catch (error) {
     console.error(error)

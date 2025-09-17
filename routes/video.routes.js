28a40075e45643271f9864f7f4456ab329e0844a
decokche <rmudi24@gmail.com>
2025-09-18 02:14:10 +0530

    /delete:id delete route created successfully

--- routes/video.routes.js
+++ routes/video.routes.js
@@ -62,7 +62,7 @@ router.post("/upload", checkAuth, async (req, res) => {
   }
 })
 
-// update video
+// 👉 update video
 
 router.put("/update/:id", checkAuth, async (req, res, next) => {
   try {
@@ -108,6 +108,47 @@ router.put("/update/:id", checkAuth, async (req, res, next) => {
     res.status(200).json({
       message: "Video updated successfully",
       video,
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      error: "Something went wrong",
+      message: error.message,
+    })
+  }
+})
+
+// 👉 delete video
+router.delete("/delete/:id", checkAuth, async (req, res) => {
+  try {
+    const videoId = req.params.id
+
+    let video = await Video.findById(videoId)
+
+    if (!videoId) {
+      return res.status(400).json({
+        error: "Video not found",
+      })
+    }
+
+    // ussi user ka hi video hai ki nhi
+    if (video.user_id.toString() !== req.user_id.toString()) {
+      return res.status(403).json({
+        error: "Unauthorization",
+      })
+    }
+
+    // Delete from cloudinary
+    await cloudinary.uploader.destroy(video.videoId, {
+      resource: "video",
+    })
+
+    await cloudinary.uploader.destroy(video.thumbnailId)
+
+    await Video.findByIdAndDelete(videoId)
+
+    res.status(200).json({
+      message: "Video deleted successfully",
     })
   } catch (error) {
     console.log(error)

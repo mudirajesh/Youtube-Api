b673eee1fd21761c0477c5c05c0cc589a7d22e6d
decokche <rmudi24@gmail.com>
2025-09-27 19:02:12 +0530

    delete comment

--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -37,4 +37,37 @@ router.post("/new", checkAuth, async (req, res) => {
   }
 })
 
+//delete comment
+router.delete("/:commentId", checkAuth, async (req, res) => {
+  try {
+    const { commentId } = req.params
+
+    const comment = await Comment.findById(commentId)
+
+    if (!comment) {
+      return res.status(404).json({
+        error: "Comment not found",
+      })
+    }
+
+    if (comment.user_id.toString() !== req.user._id) {
+      return res.status(403).json({
+        error: "Unauthorized to delete this comment",
+      })
+    }
+
+    await Comment.findByIdAndDelete(commentId)
+
+    res.status(200).json({
+      message: "Comment Deleted Successfully❌",
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      error: "Something went wrong",
+      message: error.message,
+    })
+  }
+})
+
 export default router
